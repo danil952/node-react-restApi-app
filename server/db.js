82b122cec4f4a9b739f2e78788ca30729c328833
danil952 <42176212+danil952@users.noreply.github.com>
2021-04-19 14:25:41 +0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,6 +3,6 @@ module.exports = exports = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'EventApp',
-    password: '3005952',
+    password: '123456',
     port: 1345,
 });
